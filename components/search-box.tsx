1f6d94f9ef9811e4bd8fc7158a42b7373a69f067
Prime <primecortex@outlook.com>
2026-02-25 18:27:42 +0600

    fixed search bar

--- components/search-box.tsx
+++ components/search-box.tsx
@@ -16,7 +16,10 @@ export function SearchBox() {
 
   return (
     <form onSubmit={handleSearch} className="mb-4 ring-1 focus-within:ring-2 rounded flex items-center gap-2 px-3 font-sm p-2 w-full">
-      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round">
+        <path d="m21 21-4.34-4.34" />
+        <circle cx="11" cy="11" r="8" />
+      </svg>
       <input
         type="text"
         placeholder="Search..."
